d54ddb979d2ba20da23609f47f72235c96cbf77f
elecfire2022 <99738346+elecfire2022@users.noreply.github.com>
2025-02-15 21:15:41 +0800

    Deploy dist files 1

---
+++ assets/Page2-DPFaxpUN.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,m as n}from"./index-DBKJjhjt.js";const o={};function r(t,a){return c(),n("div",null,"2")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ assets/test-DoHWRqLJ.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index-DBKJjhjt.js";const e={};function r(c,_){return" hihi! "}const s=t(e,[["render",r]]);export{s as default};
